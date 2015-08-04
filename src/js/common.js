efcebd1aaa11605865c69052b19e240672206a6f
Defiancefew <Severancepwns@gmail.com>
2015-08-04 19:59:47 +0300

    Finished basic layout

---
+++ src/js/common.js
@@ -0,0 +1,140 @@
+$(document).ready(function() {
+	
+//Animate
+(function($) {
+		$.fn.animated = function(inEffect, outEffect) {
+				$(this).css("opacity", "0").addClass("animated").waypoint(function(dir) {
+						if (dir === "down") {
+								$(this).removeClass(outEffect).addClass(inEffect).css("opacity", "1");
+						} else {
+								$(this).removeClass(inEffect).addClass(outEffect).css("opacity", "1");
+						};
+				}, {
+						offset: "70%"
+				}).waypoint(function(dir) {
+						if (dir === "down") {
+								$(this).removeClass(inEffect).addClass(outEffect).css("opacity", "1");
+						} else {
+								$(this).removeClass(outEffect).addClass(inEffect).css("opacity", "1");
+						};
+				}, {
+						offset: -$(window).height()
+				});
+		};
+})(jQuery);
+
+
+// $(".").animated("fadeIn","fadeOut");
+
+
+
+
+// Stellar
+var isMobile = {
+    Android: function() {
+        return navigator.userAgent.match(/Android/i);
+    },
+    BlackBerry: function() {
+        return navigator.userAgent.match(/BlackBerry/i);
+    },
+    iOS: function() {
+        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+    },
+    Opera: function() {
+        return navigator.userAgent.match(/Opera Mini/i);
+    },
+    Windows: function() {
+        return navigator.userAgent.match(/IEMobile/i);
+    },
+    any: function() {
+        return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+    }
+};
+
+jQuery(document).ready(function(){
+if( !isMobile.any()){
+    $.stellar({
+    	verticalOffset: 250,
+    	hideDistantElements: false
+    });
+
+    $(".header__jumbotron").animated("fadeInLeft", "fadeInRight");
+
+		$(".journal__tile-img").animated("fadeInLeft", null);
+		$(".journal__tile-img--1").animated("fadeInLeft", null);
+		$(".journal__tile-img--2").animated("fadeInLeft", null);
+		
+		
+		$(".gallery__block--1").animated("fadeInDown", "fadeInOut");
+		$(".gallery__block--2").animated("fadeInUp", "fadeInOut");
+		$(".gallery__block--3").animated("fadeInDown", "fadeInOut");
+		
+		$(".clients__left").animated("fadeInLeft", "fadeInOut");
+		$(".clients__right").animated("fadeInRight", "fadeInOut");
+		$(".clients__form-wrapper").animated("fadeIn", "fadeOut");
+    }
+
+});
+// Smooth scroll
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
+
+	//Цели для Яндекс.Метрики и Google Analytics
+	$(".count_element").on("click", (function() {
+		ga("send", "event", "goal", "goal");
+		yaCounterXXXXXXXX.reachGoal("goal");
+		return true;
+	}));
+
+	//SVG Fallback
+
+
+	//Аякс отправка форм
+	//Документация: http://api.jquery.com/jquery.ajax/
+	$("#form").submit(function() {
+		$.ajax({
+			type: "POST",
+			url: "mail.php",
+			data: $(this).serialize()
+		}).done(function() {
+			alert("Спасибо за заявку!");
+			setTimeout(function() {
+				
+				$("#form").trigger("reset");
+			}, 1000);
+		});
+		return false;
+	});
+
+	//Chrome Smooth Scroll
+	try {
+		$.browserSelector();
+		if($("html").hasClass("chrome")) {
+			$.smoothScroll();
+		}
+	} catch(err) {
+
+	};
+// Отмена перетаскивания изображений
+	$("img, a").on("dragstart", function(event) { event.preventDefault(); });
+
+});
+// Preloader
+$(window).load(function() { 
+	$(".loader_Inner").fadeOut(); 
+	$(".loader").delay(400).fadeOut("slow"); 
+});
+
+
